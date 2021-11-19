3ebf4e71c5a3b599ef15278058eaa2d851fe3fe3
eisuke <izkyyy@gmail.com>
2021-11-18 16:33:17 -0800

    filter usr elements by id without input validation

--- index.acekay.js
+++ index.acekay.js
@@ -10,24 +10,51 @@ const getUsers = async (url) => {
 }
 
 const renderUsers = (users) => {
-  const e_userTmplt = document.getElementById('user-wrapper');
+  /** TODO:
+   * render in reverse order?
+   */
+  const e_userTmplt = document.querySelector('.user-wrapper');
+  let   e_users = [];
   
   for (let i = 0; i < users.length; i++) {
     let e_newUser = e_userTmplt.cloneNode(true);
 
+    e_newUser.id = users[i].id;
     e_newUser.querySelector('#name-of-user').innerHTML = `${users[i].name}`;
     e_newUser.querySelector('#username').innerHTML = `${users[i].username}`;
     e_newUser.querySelector('#user-email').innerHTML = `${users[i].email}`;
     e_newUser.style.display = 'block';
     e_userTmplt.after(e_newUser);
+    e_users.push(e_newUser);
   }
+  e_userTmplt.remove();
+
+  return e_users;
+}
+
+const handleFilter = (e_users) => {
+  /** TODO:
+   * implement input validation
+   * https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+   */
+  const e_fltrBtn = document.getElementById('filter-btn');
+
+  e_fltrBtn.addEventListener('click', (evt) => {
+    console.log('WARNING: input value has not been validated.');
+    const fltrByThis = document.getElementById('filter-by').value;
+
+    for (let i = 0; i < e_users.length; i++) {
+      if (e_users[i].id != fltrByThis) {
+        e_users[i].style.display = 'none';
+      }
+    }
+  })
 }
 
 const main = async () => {
   const dataEndPoint = 'https://jsonplaceholder.typicode.com/users';
-
-  renderUsers(await getUsers(dataEndPoint));
-  // handleFilter();
+  const e_users = renderUsers(await getUsers(dataEndPoint));
+  handleFilter(e_users);
   // handleEdit();
   // handleDelete();
 }
